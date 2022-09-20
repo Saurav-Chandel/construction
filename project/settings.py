83bdc8f26d21b0f2db367ab8d3ff4681bfc0c7c5
Saurav-Chandel <chandelsaurav0817@gmail.com>
2022-09-20 14:32:33 +0530

    l

--- project/settings.py
+++ project/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'construction',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
